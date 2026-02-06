37584ba6fc2931db450bef9f19a0eef0ed4a9016
wenzhang1 <wenzhang1@arizona.edu>
2026-02-06 13:50:45 -0700

    Figure3_plot

--- Figure4.py
+++ Figure4.py
@@ -1,137 +1,145 @@
 from __Global__ import *
 
+class multiregression:
+    def run(self):
+        self.statistic_corr_boxplot()
+    def statistic_corr_boxplot(self):
+        """
+        绘制 partial correlation 的分布（仅针对 CVLAI 上升区域）
+        显示 sensitivity (γ), CV_inter, CV_intra 的箱线图
+        """
 
-def statistic_corr_boxplot(self):
-    """
-    绘制 partial correlation 的分布（仅针对 CVLAI 上升区域）
-    显示 sensitivity (γ), CV_inter, CV_intra 的箱线图
-    """
+        # === 1. 读取数据 ===
+        dff = result_root + rf'\partial_correlation\Dataframe\\1mm_new\\Obs.df'
+        df = T.load_df(dff)
+        df = self.df_clean(df)
+        print(len(df))
 
-    # === 1. 读取数据 ===
-    dff = result_root + rf'\partial_correlation\Dataframe\\1mm_new\\Obs.df'
-    df = T.load_df(dff)
-    df = self.df_clean(df)
-    print(len(df))
+        # === 仅保留CVLAI显著上升的像素 ===
 
-    # === 仅保留CVLAI显著上升的像素 ===
+        # === 2. 变量设置 ===
+        variable_list = [
+            'sensitivity',
+            'Precip_sum_detrend_CV',
+            'CV_daily_rainfall_average',
+        ]
 
-    # === 2. 变量设置 ===
-    variable_list = [
-        'sensitivity',
-        'Precip_sum_detrend_CV',
-        'CV_daily_rainfall_average',
-    ]
+        label_dic = {
+            'sensitivity': r'$\gamma$',
+            'Precip_sum_detrend_CV': r'$CV_{inter}$',
+            'CV_daily_rainfall_average': r'$CV_{intra}$',
+        }
 
-    label_dic = {
-        'sensitivity': r'$\gamma$',
-        'Precip_sum_detrend_CV': r'$CV_{inter}$',
-        'CV_daily_rainfall_average': r'$CV_{intra}$',
-    }
+        # === 4. 数据提取 ===
 
-    # === 4. 数据提取 ===
-
-    for model in self.model_list:
-        if not 'composite_LAI_median' in model:
-            continue
-
-        result_dic = {}
-
-        # print(len(df));exit()
-        for variable in variable_list:
-            new_variable = f'{model}_{variable}_sig'
-            if new_variable not in df.columns:
+        for model in self.model_list:
+            if not 'composite_LAI_median' in model:
                 continue
 
-            vals = np.array(df[new_variable].tolist(), dtype=float)
-            vals[(vals > 99) | (vals < -99)] = np.nan
-            vals = vals[~np.isnan(vals)]
-            print(f'{variable}', len(vals))
+            result_dic = {}
 
-            #     plt.hist(vals, bins=30)
-            #     plt.axvline(np.mean(vals), color='g', label='Mean')
-            #     plt.axvline(np.median(vals), color='r', label='Median')
-            #     plt.legend()
-            #     plt.show()
+            # print(len(df));exit()
+            for variable in variable_list:
+                new_variable = f'{model}_{variable}_sig'
+                if new_variable not in df.columns:
+                    continue
 
-            # vals_mean=np.nanmean(vals)
-            # print(vals_mean)
-            result_dic[new_variable] = vals
+                vals = np.array(df[new_variable].tolist(), dtype=float)
+                vals[(vals > 99) | (vals < -99)] = np.nan
+                vals = vals[~np.isnan(vals)]
+                print(f'{variable}', len(vals))
 
-        # === 5. 按 variable_list 顺序组织数据 ===
-        data_list = []
-        x_labels = []
+                #     plt.hist(vals, bins=30)
+                #     plt.axvline(np.mean(vals), color='g', label='Mean')
+                #     plt.axvline(np.median(vals), color='r', label='Median')
+                #     plt.legend()
+                #     plt.show()
 
-        for var in variable_list:
-            key = f'{model}_{var}_sig'
-            if key in result_dic:
-                data_list.append(result_dic[key])
-                x_labels.append(label_dic[var])
+                # vals_mean=np.nanmean(vals)
+                # print(vals_mean)
+                result_dic[new_variable] = vals
 
-                # 设置颜色
-        color_list = ['#a577ad', 'yellowgreen', 'Pink', '#f599a1']
-        dark_colors = ['#774685', 'Olive', 'Salmon', '#c3646f']  # 可以改为你自定义的 darken_color 函数
+            # === 5. 按 variable_list 顺序组织数据 ===
+            data_list = []
+            x_labels = []
 
-        # 绘图
-        fig, ax = plt.subplots(figsize=(4, 3))
+            for var in variable_list:
+                key = f'{model}_{var}_sig'
+                if key in result_dic:
+                    data_list.append(result_dic[key])
+                    x_labels.append(label_dic[var])
 
-        box = ax.boxplot(
-            data_list,
-            patch_artist=True,
-            widths=0.4,
-            showfliers=False,
+                    # 设置颜色
+            color_list = ['#a577ad', 'yellowgreen', 'Pink', '#f599a1']
+            dark_colors = ['#774685', 'Olive', 'Salmon', '#c3646f']  # 可以改为你自定义的 darken_color 函数
 
-            showmeans=False,
+            # 绘图
+            fig, ax = plt.subplots(figsize=(4, 3))
 
-        )
+            box = ax.boxplot(
+                data_list,
+                patch_artist=True,
+                widths=0.4,
+                showfliers=False,
 
-        # 自定义颜色
-        # === 美化箱线图（让 median、whisker 与箱体颜色一致） ===
-        for i, patch in enumerate(box['boxes']):
-            face_color = color_list[i]
-            edge_color = dark_colors[i]
+                showmeans=False,
 
-            # 箱体
-            patch.set_facecolor(face_color)
-            patch.set_edgecolor(edge_color)
-            patch.set_linewidth(1.5)
+            )
 
-            # 中位线
-            box['medians'][i].set_color(edge_color)
-            box['medians'][i].set_linewidth(1.8)
+            # 自定义颜色
+            # === 美化箱线图（让 median、whisker 与箱体颜色一致） ===
+            for i, patch in enumerate(box['boxes']):
+                face_color = color_list[i]
+                edge_color = dark_colors[i]
 
-            # 上下须（whisker）
-            box['whiskers'][2 * i].set_color(edge_color)
-            box['whiskers'][2 * i + 1].set_color(edge_color)
-            box['whiskers'][2 * i].set_linewidth(1.2)
-            box['whiskers'][2 * i + 1].set_linewidth(1.2)
+                # 箱体
+                patch.set_facecolor(face_color)
+                patch.set_edgecolor(edge_color)
+                patch.set_linewidth(1.5)
 
-            # 顶部和底部横线（caps）
-            box['caps'][2 * i].set_color(edge_color)
-            box['caps'][2 * i + 1].set_color(edge_color)
-            box['caps'][2 * i].set_linewidth(1.2)
-            box['caps'][2 * i + 1].set_linewidth(1.2)
+                # 中位线
+                box['medians'][i].set_color(edge_color)
+                box['medians'][i].set_linewidth(1.8)
 
-        # 设置x轴
+                # 上下须（whisker）
+                box['whiskers'][2 * i].set_color(edge_color)
+                box['whiskers'][2 * i + 1].set_color(edge_color)
+                box['whiskers'][2 * i].set_linewidth(1.2)
+                box['whiskers'][2 * i + 1].set_linewidth(1.2)
 
-        plt.xticks(range(1, len(x_labels) + 1), x_labels, fontsize=10)
-        plt.xlabel('')
-        plt.ylabel('Partial correlation', fontsize=10)
+                # 顶部和底部横线（caps）
+                box['caps'][2 * i].set_color(edge_color)
+                box['caps'][2 * i + 1].set_color(edge_color)
+                box['caps'][2 * i].set_linewidth(1.2)
+                box['caps'][2 * i + 1].set_linewidth(1.2)
 
-        plt.axhline(0, color='gray', linestyle='--')
-        # plt.tight_layout()
-        # plt.show()
+            # 设置x轴
 
-        outdir = result_root + rf'\FIGURE\SI\\'
-        Tools().mk_dir(outdir, force=True)
+            plt.xticks(range(1, len(x_labels) + 1), x_labels, fontsize=10)
+            plt.xlabel('')
+            plt.ylabel('Partial correlation', fontsize=10)
 
-        # outf=join(outdir,f'{model}_partial_correlation_boxplot_3mm.pdf')
-        # plt.savefig(outf,bbox_inches='tight',dpi=300
-        #
-        # )
-        # plt.close()
+            plt.axhline(0, color='gray', linestyle='--')
+            # plt.tight_layout()
+            # plt.show()
+
+            outdir = result_root + rf'\FIGURE\SI\\'
+            Tools().mk_dir(outdir, force=True)
+
+            # outf=join(outdir,f'{model}_partial_correlation_boxplot_3mm.pdf')
+            # plt.savefig(outf,bbox_inches='tight',dpi=300
+            #
+            # )
+            # plt.close()
+
+
+
+
+
 def main():
+    multiregression().run()
 
-    statistic_corr_boxplot()
+
     pass
 
 if __name__ == '__main__':
